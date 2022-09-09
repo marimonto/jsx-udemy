32c9e46d3d11e6341904959531593b0f78f181d0
marimontod-kc <mariana.montoya@kinandcarta.com>
2022-09-09 09:48:17 -0500

    Building Content with JSX

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const el = document.getElementById('root');
+
+const root = ReactDOM.createRoot(el);
+
+const App = () => {
+    return <textarea autoFocus />;
+}
+
+root.render(<App />);
